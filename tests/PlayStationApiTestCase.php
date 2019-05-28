f3b986e9c2d69bb22a9c2769d0fa99ad3b9b160e
Tustin <Tustin@users.noreply.github.com>
2019-05-27 19:19:58 -0500

    add autoload dev namespace for tests, simplify redundant code and be sensible with image
    I thought I was being clever by just saving all the image bytes in the test instead of loading the image like a sensible person. Let's change that.

---
+++ tests/PlayStationApiTestCase.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PlayStation\Tests;
+
+use PlayStation\Client;
+
+class PlayStationApiTestCase extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * PlayStation Client
+     *
+     * @var PlayStation\Client
+     */
+    protected static $client;
+
+    /**
+     * The logged in user
+     *
+     * @var PlayStation\Api\User
+     */
+    protected static $loggedInUser;
+
+    /**
+     * Test User
+     *
+     * @var PlayStation\Api\User
+     */
+    protected static $testUser;
+
+    /**
+     * My PSN account
+     * 
+     * My account shouldn't have any privacy settings enabled which should allow all API calls to succeed.
+     *
+     * @var PlayStation\Api\User
+     */
+    protected static $tustinUser;
+
+    public static function setUpBeforeClass()
+    {
+        self::$client = new Client(['verify' => false, 'proxy' => '127.0.0.1:8888']);
+
+        $refreshToken = getenv('PSN_PHP_REFRESH_TOKEN');
+
+        self::$client->login($refreshToken);
+
+        self::$loggedInUser = self::$client->user();
+
+        self::$testUser = self::$client->user('Test');
+
+        self::$tustinUser = self::$client->user('tustin25');
+    }
+}
