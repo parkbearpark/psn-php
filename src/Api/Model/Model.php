d2b996d1e68a649c76dd6f174be308d95a46f751
Tustin <Tustin@users.noreply.github.com>
2020-02-08 20:57:18 -0600

    add setCache to Model

--- src/Api/Model/Model.php
+++ src/Api/Model/Model.php
@@ -7,4 +7,11 @@ use Tustin\PlayStation\Api\Api;
 abstract class Model extends Api
 {
     protected ?object $cache = null;
+
+    public function setCache(object $data) : self
+    {
+        $this->cache = $data;
+
+        return $this;
+    }
 }
