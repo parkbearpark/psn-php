26b7ab44f7044b93238110dfb428d39c07f3324d
yuma <yumai6205@gmail.com>
2021-02-27 19:16:59 +0900

    loginWithNpssoは動いてない

--- src/Client.php
+++ src/Client.php
@@ -17,32 +17,15 @@ class Client extends AbstractClient
     private const VERSION = 'dev-3.0.0';
     // The client id and client secret are for the iOS PlayStation app.
     // These cannot be generated at this time; therefore, we have to use their permissions scopes as well.
-    private const CLIENT_ID     = 'ebee17ac-99fd-487c-9b1e-18ef50c39ab5';
-    private const CLIENT_SECRET = 'e4Ru_s*LrL4_B2BD';
+    private const CLIENT_ID     = 'ac8d161a-d966-4728-b0ea-ffec22f69edc';
     private const SCOPES = [
-        'kamaji:activity_feed_get_news_feed',
-        'kamaji:activity_feed_internal_feed_submit_story',
-        'kamaji:activity_feed_set_feed_privacy',
-        'kamaji:activity_feed_submit_feed_story',
-        'kamaji:communities',
-        'kamaji:game_list',
-        'kamaji:get_account_hash',
-        'kamaji:get_players_met',
-        'kamaji:satchel',
-        'kamaji:satchel_delete',
-        'kamaji:ugc:distributor',
-        'oauth:manage_device_usercodes',
-        'psn:sceapp',
-        'user:account.attributes.validate',
-        'user:account.profile.get',
-        'user:account.profile.update',
-        'user:account.settings.privacy.get'
+        'psn:clientapp', 'psn:mobile.v1'
     ];
 
     private $accessToken;
 
     private $refreshToken;
-    
+
     private $expiresAt;
 
     public function __construct(array $guzzleOptions = [])
@@ -50,13 +33,13 @@ class Client extends AbstractClient
         // We can't really use a base_uri here because Sony sucks.
         $guzzleOptions['allow_redirects'] = false;
         $guzzleOptions['headers']['User-Agent'] = 'psn-php/' . self::VERSION;
- 
+
         parent::__construct($guzzleOptions);
     }
 
     /**
      * Login using a npsso token.
-     * 
+     *
      * @see https://tusticles.com/psn-php/first_login.html
      *
      * @param string $npsso
@@ -64,11 +47,11 @@ class Client extends AbstractClient
      */
     public function loginWithNpsso(string $npsso) : void
     {
-        $response = $this->post('https://auth.api.sonyentertainmentnetwork.com/2.0/oauth/token', [
+        $response = $this->post('https://ca.account.sony.com/api/authz/v3/oauth/token', [
             'client_id' => self::CLIENT_ID,
-            'client_secret' => self::CLIENT_SECRET,
             'scope' => implode(' ', self::SCOPES),
             'grant_type' => 'sso_cookie',
+            'access_type' => 'offline'
         ], [ 'Cookie' => 'npsso=' . $npsso ]);
 
         $this->postLogin($response);
@@ -76,7 +59,7 @@ class Client extends AbstractClient
 
     /**
      * Login using an existing refresh token.
-     * 
+     *
      * @see https://tusticles.com/psn-php/future_logins.html
      *
      * @param string $refreshToken
@@ -84,9 +67,8 @@ class Client extends AbstractClient
      */
     public function loginWithRefreshToken(string $refreshToken) : void
     {
-        $response = $this->post('https://auth.api.sonyentertainmentnetwork.com/2.0/oauth/token', [
+        $response = $this->post('https://ca.account.sony.com/api/authz/v3/oauth/token', [
             'client_id' => self::CLIENT_ID,
-            'client_secret' => self::CLIENT_SECRET,
             'scope' => implode(' ', self::SCOPES),
             'grant_type' => 'refresh_token',
             'refresh_token' => $refreshToken
@@ -97,7 +79,7 @@ class Client extends AbstractClient
 
     /**
      * Access the PlayStation API using an existing access token.
-     * 
+     *
      * @see https://tusticles.com/psn-php/future_logins.html
      *
      * @param string $accessToken
@@ -124,7 +106,7 @@ class Client extends AbstractClient
         $this->refreshToken = $response->refresh_token;
         $this->expiresAt = Carbon::now()->addSeconds($response->expires_in);
     }
-    
+
     /**
      * Gets the current access token.
      *
